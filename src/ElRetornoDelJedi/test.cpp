b9cdd2c8c2b965d53e62c8fdfc57d48bf25a9e81
Iván Pondal <ivan.pondal@gmail.com>
2016-05-08 18:26:33 -0300

    Refactor ex3 variable names and added report sample test

--- src/ElRetornoDelJedi/test.cpp
+++ src/ElRetornoDelJedi/test.cpp
@@ -83,9 +83,31 @@ void test_3x3() {
 
 }
 
+void test_3x3_informe() {
+	int N = 3;
+	int M = 3;
+	int H = 1;
+	vector < vector <int> > E = vector < vector <int> >(N, vector <int> (M,0));
+	//   0  | 10 | 5
+	//   15 | 15 | 5
+	//   5  | 10 | 20
+	E[0][0] = 4 ; E[0][1] = -2; E[0][2] = -1;
+	E[1][0] = 6; E[1][1] = 7; E[1][2] = -5;
+	E[2][0] = 0 ; E[2][1] = 3; E[2][2] = 4;
+	vector < vector < pair < int, char > > > DP;
+	pair < int, vector <char>  > solucion = resolver(N,M,H,E,DP);
+	vector <char> camino ;
+	camino.push_back('Y'); camino.push_back('X');camino.push_back('Y'); camino.push_back('X');
+	pair < int, vector <char>  > esperado = make_pair(4, camino);
+	ASSERT_EQ(esperado.first, solucion.first);
+	// imprimirCamino(solucion.second);
+	ASSERT(compararVectores(esperado.second, solucion.second));
+
+}
 void correr_tests_unitarios() {
     RUN_TEST(test_unico);
     RUN_TEST(test_fila);
     RUN_TEST(test_columna);
     RUN_TEST(test_3x3);
+    RUN_TEST(test_3x3_informe);
 }
