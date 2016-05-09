5c6b7ec65261f98cf1f0fff578d1c1502ecd079a
Franco Frizzo <francofrizzo@gmail.com>
2016-05-09 11:40:45 -0300

    Add functions for comparing and printing vectors

--- src/main.h
+++ src/main.h
@@ -6,6 +6,7 @@
 #include <fstream>       // open, close, <<
 #include <iomanip>       // stoi
 #include <unistd.h>      // getopt
+#include <vector>        // vector
 
 // Medición de tiempos
 static std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
@@ -20,5 +21,33 @@ extern bool verbose;
 void correr_solucion();
 void correr_tests_unitarios();
 void correr_pruebas_performance();
+
+// Compara dos vectores
+template <typename T>
+bool compararVectores(const std::vector<T> v1, const std::vector<T> v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    } else {
+        for (unsigned int i = 0; i < v1.size(); i++) {
+            if (v1[i] != v2[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Imprime un vector
+template <typename T>
+std::ostream& operator <<(std::ostream& os, const std::vector<T>& v) {
+    os << "[";
+    for (typename std::vector<T>::const_iterator it = v.begin();
+        it != v.end(); ++it)
+    {
+        os << *it << ", ";
+    }
+    os << "\b\b]";
+    return os;
+}
 
 #endif
