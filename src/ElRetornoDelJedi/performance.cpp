ce0637a2e41bab2beb9923b67a7e479af9c16d32
Franco Frizzo <francofrizzo@gmail.com>
2016-05-09 09:58:36 -0300

    Revert "ej3: start code."
    So that code branch 'master' compiles.
    
    This reverts commit 5ccb6b4e8db91cd0a9da191ff79b9fb945851388.

--- src/ElRetornoDelJedi/performance.cpp
+++ src/ElRetornoDelJedi/performance.cpp
@@ -5,38 +5,7 @@
 **  Ejercicio 3: El retorno del (que te) jedi
 **  Pruebas de performance
 */
-int N,M,H;
-vector< vector <int> > E;
-vector< vector <int> > DP;
 
 void correr_pruebas_performance() {
-    // levanto el input
-	cin << N << M << H;
-	E = vector< vector<int>>(N, vector<int>(M));
-	for (int i = 0; i < N; ++i){
-		for (int j = 0; j < M; ++j){
-			cin << E[i][j] ;
-		}
-	}
-	
-	// Instacio el DP
-	DP = vector< vector<int>>(N, vector<int>(M));
-	DP[0][0] = 0;
-	for (int i = 1; i < N; ++i){
-		int delta = math.abs(E[i][0] - E[i-1][0]) 
-		if(H < delta){
-			DP[i][0] =  (delta - H) + DP[i-1][0];
-		}else{
-			DP[i][0] = DP[i-1][0];
-		}
-	}
-	for (int i = 1; i < N; ++i){
-		int delta = math.abs(E[0][i] - E[0][i-1]) 
-		if(H < delta){
-			DP[0][i] =  (delta - H) + DP[0][i-1];
-		}else{
-			DP[0][i] = DP[0][i-1];
-		}
-	}
-	//
+    std::cout << "Acá todavía no hay nada" << std::endl;
 }
